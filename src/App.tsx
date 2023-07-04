f63c0919c19a0e048200d9775816cf2220c7c06c
James <mirageapache@gmail.com>
2023-07-04 10:39:27 +0800

    Setting gh-pages deploy

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,10 @@ import 'styles/css/reset.css';
 import 'styles/css/base.css';
 
 function App() {
+  const basename = process.env.PUBLIC_URL;
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename={basename}>
         <Routes>
           <Route path='*' element={<HomePage />}></Route>
         </Routes>
